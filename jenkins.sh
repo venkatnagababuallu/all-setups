5aa6b3d0baa995af9e813dd2ba21090050291a97
venkatnagababuallu <venkatanagababuallu@gmail.com>
2026-02-01 16:11:09 +0530

    Update Jenkins repository URL in jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,6 @@
 sudo yum update –y
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/rpm-stable/jenkins.repo
 sudo yum upgrade
 sudo yum install java-17-amazon-corretto -y
 sudo yum install jenkins git -y
